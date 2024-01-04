0d78442bdbef8e613b0a1643f7ea9876e9e5e159
Amoh-Ntim <amohntim123@gmail.com>
2024-01-04 13:56:50 +0000

    edit posts

--- frontend/blogsite/src/components/EditCard.jsx
+++ frontend/blogsite/src/components/EditCard.jsx
@@ -90,7 +90,7 @@ const EditCard = () => {
       {/* ...similar fields for excerpt and content... */}
       <Link to="/postcards">
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleEdit}>
-        Submit
+        Edit Post
       </button>
       </Link>
     </form>
