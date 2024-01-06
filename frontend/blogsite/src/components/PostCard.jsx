2a64facd8f8f11ac8152b706a3e2cc8956680cbf
Amoh-Ntim <amohntim123@gmail.com>
2024-01-06 09:25:23 +0000

    cleared all comments

--- frontend/blogsite/src/components/PostCard.jsx
+++ frontend/blogsite/src/components/PostCard.jsx
@@ -61,27 +61,3 @@ const [error, setError] = useState(null);
 };    
 
 export default PostCard;
-// <PostCard key={post.id} {...post} />
-
-
-// <div className="container mx-auto">
-// {isLoading ? (
-//   <Spinner/>
-// ) : (
-//   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-//     {posts.length > 0 ? (
-//       posts.map((post) => (
-//         <div key={post._id} className="bg-white shadow-md rounded-lg overflow-hidden">
-//   <img className="w-full" src={post.url} alt={post.title} />
-//   <div className="p-4">
-//       <h5 className="text-xl font-semibold">{post.title}</h5>
-//       <p className="text-gray-700">{post.publishYear}</p> <a href={`/posts/${post._id}`} className="text-blue-500 hover:underline">Read More</a>
-//   </div>
-// </div> 
-//       ))
-//     ) : (
-//       <div className="animate-ping flex justify-center items-center w-16 h-16 m-8 rounded-full bg-sky-600"></div>
-//     )}
-//   </div>
-// )}
-// </div>
