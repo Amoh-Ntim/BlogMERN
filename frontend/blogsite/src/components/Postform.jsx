1f1846548097c08a0638c8a3bb33e4f3426e6f78
Amoh-Ntim <amohntim123@gmail.com>
2024-01-23 10:34:07 +0000

    delete

--- frontend/blogsite/src/components/Postform.jsx
+++ frontend/blogsite/src/components/Postform.jsx
@@ -1,5 +1,5 @@
 import { useState} from 'react';
-import axios from 'axios'; // Import Axios
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
