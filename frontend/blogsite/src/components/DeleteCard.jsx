9e6c00693295b554c60322fbd6e069156929ab08
Amoh-Ntim <amohntim123@gmail.com>
2024-01-25 20:55:33 +0000

    delete

--- frontend/blogsite/src/components/DeleteCard.jsx
+++ frontend/blogsite/src/components/DeleteCard.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { useState } from 'react';
-// import Modal from 'react-modal';
 import { useNavigate, useParams } from 'react-router-dom';
 
 const DeleteCard = () => {
