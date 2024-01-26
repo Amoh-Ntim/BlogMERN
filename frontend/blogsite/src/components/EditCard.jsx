42402876612c62dba106ba1e3a4f0a5d3269094d
Amoh-Ntim <amohntim123@gmail.com>
2024-01-26 21:35:13 +0000

    editcard

--- frontend/blogsite/src/components/EditCard.jsx
+++ frontend/blogsite/src/components/EditCard.jsx
@@ -82,7 +82,6 @@ const EditCard = () => {
           onChange={(e) => setPublishYear(e.target.value)}
         />
       </div>
-      {/* ...similar fields for excerpt and content... */}
       <Link to="/postcards">
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={handleEdit}>
         Edit Post
