f1def6db8043bfc6a2f0247933660e0c65c75ba5
Amoh-Ntim <amohntim123@gmail.com>
2024-02-13 22:04:22 +0000

    parent

--- frontend/blogsite/src/components/ParentComponent.jsx
+++ frontend/blogsite/src/components/ParentComponent.jsx
@@ -7,6 +7,7 @@ import DeleteCard from "./DeleteCard";
 
 const ParentComponent = () => {
     const [posts, setPosts] = useState([]);
+    
     const updatePosts = async () => {
       try {
         const response = await axios.get('http://localhost:5000/data');
